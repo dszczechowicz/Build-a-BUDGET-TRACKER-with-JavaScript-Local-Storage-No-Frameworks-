d1444c58cdb85412d708455a18ff81246fbbc7b0
Damian <dmn.szczechowicz@gmail.com>
2021-12-16 20:52:08 +0100

    finished for day 1

--- js/BudgetTracker.js
+++ js/BudgetTracker.js
@@ -12,34 +12,32 @@ export default class BudgetTracker {
   }
 
   static html() {
-    return `
-        <table class="budget-tracker">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Type</th>
-            <th>Amount</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody class="entries">
-        </tbody>
-        <tbody>
-            <tr>
-                <td colspan="5">
-                    <button type="button" class="new-entry">New Entry</button>
-                </td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="5" class="summary">
-                    <strong>Total:</strong>
-                    <span class="total">$0.00</span>
-                </td>
-            </tr>
-        </tfoot>
+    return `<table class="budget-tracker">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody class="entries"></tbody>
+            <tbody>
+                <tr>
+                    <td colspan="5" class="controls">
+                        <button type="button" class="new-entry">New Entry</button>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="5" class="summary">
+                        <strong>Total:</strong>
+                        <span class="total">$0.00</span>
+                    </td>
+                </tr>
+            </tfoot>
       </table>
       `;
   }
@@ -73,17 +71,48 @@ export default class BudgetTracker {
         `;
   }
 
-  load() {}
+  load() {
+    const entries = JSON.parse(
+      localStorage.getItem("budget-tracker-entries") || "[]"
+    );
+
+    for (const entry in entries) {
+      this.addEntry(entry);
+    }
+
+
+    this.updateSummary();
+  }
 
   updateSummary() {}
 
   save() {}
 
-  addEntry(entry = {}) {}
+  addEntry(entry = {}) {
+      this.root.querySelector(".entries").insertAdjacentHTML("beforeend", BudgetTracker.entryHtml());
+
+      const row = this.root.querySelector(".entries tr:last-of-type");
+
+      row.querySelector(".input-date").value = entry.date || new Date().toISOString().replace(/T.*/, "");
+      row.querySelector(".input-description").value = entry.description || "";
+      row.querySelector(".input-type").value = entry.type || "income";
+      row.querySelector(".input-amount").value = entry.amount || 0;
+      row.querySelector(".delete-entry").addEventListener("click", e => {
+        this.onDeleteEntryBtnClick(e);
+      });
+
+      row.querySelectorAll(".input").forEach(input => {
+          input.addEventListener("change", () => this.save());
+      });
+  }
 
   getEntryRows() {}
 
-  onNewEntryBtnClick() {}
+  onNewEntryBtnClick() {
+      this.addEntry();
+  }
 
-  onDeleteEntryBtnClick(e) {}
+  onDeleteEntryBtnClick(e) {
+      console.log("entry deleted");
+  }
 }
